4a61acd78fe135f7d878f9f01abe52b18dbeb76b
rnartallo <moncho.nartallo@gmail.com>
2021-07-16 16:50:21 +0100

    back end should now avoid undefined error - but it still has bugs

--- src/getSolHelper.ts
+++ src/getSolHelper.ts
@@ -9,11 +9,15 @@ export default async function getSolLog(rover: string, sol: number) {
   const url = `https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${process.env.API_KEY}`;
   const { data } = await axios.get(url);
   const photos: any[] = data.photo_manifest.photos;
-  const day: any = photos[sol - 1];
+  const day: any = photos.find((photo)=> {return photo.sol===sol});
+  if (day===undefined){
+      return {rover:rover,sol:sol,total_photos :0}
+  }
+  console.log(day);
   const sollog: Sollog = {
     rover: rover,
     sol: sol,
-    total_photos: day.total_photos,
+    total_photos:day.total_photos ,
   };
   return sollog;
 }
